3403819ae60980661fba80cc46fbde25f0a2f77b
pijoneris <benasmiliunas@gmail.com>
2017-06-24 21:30:49 +0300

    ss

--- app/src/main/java/com/mabe/productions/prisiukantraste/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/mabe/productions/prisiukantraste/MyFirebaseInstanceIDService.java
@@ -4,6 +4,7 @@ package com.mabe.productions.prisiukantraste;
  * Created by Benas on 24/06/2017.
  */
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -12,36 +13,21 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
-    private static final String TAG = "TEST";
-
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the InstanceID token
-     * is initially generated so this is where you would retrieve the token.
-     */
-    // [START refresh_token]
     @Override
     public void onTokenRefresh() {
-        // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
+        Log.d("TEST", "Refreshed token: " + refreshedToken);
         sendRegistrationToServer(refreshedToken);
+        super.onTokenRefresh();
     }
-    // [END refresh_token]
 
-    /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM InstanceID token with any server-side account
-     * maintained by your application.
-     *
-     * @param token The new token.
-     */
     private void sendRegistrationToServer(String token) {
-        // TODO: Implement this method to send token to your app server.
+        SharedPreferences sharedPreferences = this.getSharedPreferences("user_data", MODE_PRIVATE);
+
+        String old_token = sharedPreferences.getString("device_token",null);
+        new ServerManager(this, ServerManager.SERVER_ADDRESS_INSERT_DEVICE_TOKEN,false,null).execute(old_token, token);
+
+        sharedPreferences.edit().putString("device_token", token).commit();
+
     }
 }
